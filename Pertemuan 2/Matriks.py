53eca710d4c03c9cf20c598b397b6e70112b51d7
ridhofadilah <rdhofdlh@ymail.com>
2017-03-29 07:05:43 +0700

    Sayid Muhammad Ridho Fadilah

--- Pertemuan 2/Matriks.py
+++ Pertemuan 2/Matriks.py
@@ -1,23 +1,35 @@
-data = [1,2,3]
-data2 = [[1,2,3],[4,5,6],[7,8,9]]
+data = [[1,2,3],[4,5,6],[7,8,9]]
+data2 = [[1],[2],[3]]
  
+matriks = []
+matriks1 =[]
+for each in data:
+	counter =0
+	isimatriks=0
+	for isi in each:
+		counter +=1
+		counter2 =0
+		for each2 in data2:
+			for isi2 in each2:
+				counter2+=1
+				if counter == counter2 :
+					isimatriks += (isi*isi2)
+	matriks1.append(isimatriks)
 
-matriks =[]
+counter  = 0
+temp1=[]
+temp2=[]
+temp3=[]
 
-for x in data2:
-	d=0
-	e=0
-	for a in x:
-		d+=1
-		c = 0
-		for b in data:
-			c += 1
-			if c == 1 and d == 1:
-				e+=(a*b)
-			elif c==2 and d==2:
-				e+= (a*b)
-			elif c==3 and d==3:
-				e+= (a*b)
-	matriks.append(e)
-
+for each in matriks1:
+	counter +=1
+	if counter==1:
+		temp1.append(each)
+	elif counter == 2:
+		temp2.append(each)
+	else:
+		temp3.append(each)
+matriks.append(temp1)
+matriks.append(temp2)
+matriks.append(temp3)
 print (matriks)
